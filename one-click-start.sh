5de7fe0031117868e1bf43d2fe6d6b8cd60f6856
Eddie Cheng <fcheng@th-wildau.de>
2021-06-20 11:32:27 +0800

    declare variables

--- one-click-start.sh
+++ one-click-start.sh
@@ -1,7 +1,11 @@
 #! /bin/zsh
 
+youDontKnowJS="https://github.com/getify/You-Dont-Know-JS"
+allAboutJS="https://github.com/chengr4/all-about-JS"
+TRANSLATOR="https://translate.google.com.tw/?hl=zh-TW&sl=auto&tl=en&op=translate"
+
 # open chrome
-open -a 'google chrome' https://github.com/getify/You-Dont-Know-JS https://github.com/chengr4/all-about-JS
+open -a 'google chrome' ${youDontKnowJS} ${allAboutJS} ${TRANSLATOR}
 
 # open typora
 open -a 'typora' README.md
