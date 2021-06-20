7feaa30c7f98692f328c33e73e255ce48d8c3b7f
Eddie Cheng <fcheng@th-wildau.de>
2021-06-20 11:03:55 +0800

    set one click start

---
+++ one-click-start.sh
@@ -0,0 +1,10 @@
+#! /bin/zsh
+
+# open chrome
+open -a 'google chrome' https://github.com/getify/You-Dont-Know-JS https://github.com/chengr4/all-about-JS
+
+# open typora
+open -a 'typora' README.md
+
+# open vscode 
+code .
