7fd84cc94607f60b56005c586a1b3ec2f6d5ff24
Eddie Cheng <fcheng@th-wildau.de>
2021-07-17 23:09:53 +0800

    nail down object methods and anonymous function expression property

--- OO/objects.js
+++ OO/objects.js
@@ -12,3 +12,18 @@ var userOne = {
 
 userOne.login();
 userOne.logout();
+
+const testMehthods = {
+  // object methods
+  coolMethod() {
+    console.log("i ate an apple");
+  }, // commas!
+
+  // (anonymous) function expression property
+  oldSchool: function () {
+    console.log("i ate nothing");
+  },
+};
+
+console.log(testMehthods.oldSchool); // do not print anything
+testMehthods.oldSchool(); // i ate nothing
